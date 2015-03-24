5718543b0676ae36dccd4553d54291a9408cec18
Vitaliy Stepanenko <mail@vitaliy.in>
2015-03-24 17:49:12 +0200

    DB configs fixed

--- config/database.php
+++ config/database.php
@@ -1,5 +1,16 @@
 <?php
 
+if ($url = getenv("CLEARDB_DATABASE_URL")) {
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+} else {
+    $host = env('DB_HOST', 'localhost');
+    $database = env('DB_DATABASE', 'forge');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+}
 return [
 
     /*
@@ -48,10 +59,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
