0647b962062fcc4babbd44f08a5a027de31c7b8a
Vitaliy Stepanenko <mail@vitaliy.in>
2015-03-24 17:57:41 +0200

    db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,20 +1,35 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+        $faker = Faker\Factory::create();
 
+        for ($i = 1; $i <= 2000; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $faker->city,
+                'birthday' => $faker->date(),
+                'phone_number' => $faker->phoneNumber,
+                'country' => $faker->country,
+                'company' => $faker->company,
+                'posts_count' => rand(0, 50),
+                'comments_count' => rand(0, 1000),
+            ]);
+        }
+    }
 }
