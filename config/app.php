5196d29fe68d28179ba792ca1865a21adc4e6eba
Vitaliy Stepanenko <mail@vitaliy.in>
2017-07-18 13:32:39 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
 	'key' => env('APP_KEY', 'cDvwHUpRwcfSjqMdL37K5rFL3fVPeYfH'),
 
-	'cipher' => MCRYPT_RIJNDAEL_128,
+	'cipher' => 'AES-256-CBC',
 
 	/*
 	|--------------------------------------------------------------------------
